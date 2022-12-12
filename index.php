b41cd3df04ba1118d7b484cb5a86b2121f7623f4
Floz8 <55977382+Floz8@users.noreply.github.com>
2022-12-12 15:26:07 +0100

    Ajout du style

--- index.php
+++ index.php
@@ -8,16 +8,47 @@
     <title>FizzBuzz</title>
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  
+<style>
+        body {
+            text-align: center;
+            font-family: sans-serif;
+        }
+        .fizzbuzz-output {
+            margin: auto;
+            text-align: left;
+            width: 400px;
+            border: 1px solid #ccc;
+            padding: 20px;
+        }
+        input[type="number"] {
+            padding: 10px;
+            font-size: 16px;
+        }
+        button {
+            padding: 10px 20px;
+            font-size: 16px;
+            background-color: #ccc;
+            border: none;
+            cursor: pointer;
+        }
+        button:hover {
+            background-color: #ddd;
+        }
+    </style>
 </head>
 <header>
 <h1>FIZZBUZZ </h1>    
 
 </header>
 <body>
-    <!-- <form action="resultat.php" method="post"></form> -->
-    <input type="number" id="number" name="number" min="0" oninput="validity.valid||(value='');"> 
+   
+
+    <input type="number" id="number" name="number" min="0" value="0" oninput="validity.valid||(value='');" required> 
     <button class="btn btn-primary" onClick="FizzbuzzHTML()">test</button>
+
+   
+    <!-- <form action="resultat.php" method="post"></form> -->
+   
     
     <div id="resultat">
     <p> Resultat: </p>        
